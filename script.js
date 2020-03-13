cfc3693a06cc8ec69488ea4932db94aa2cf25144
Safo <s.i.post@mai.ru>
2020-03-13 22:47:39 +0500

    sendForm do not work

--- script.js
+++ script.js
@@ -274,10 +274,50 @@ const sendForm = () => {
 
   const statusMessage = document.createElement('div');
   statusMessage.classList.add('status-message');
-  statusMessage.style.cssText = `font-size: 14px;`;
-  form.appendChild(statusMessage);
-  statusMessage.textContent = loadMessage;
+  statusMessage.style.cssText = `font-size: 16px;
+    font-family: 'MuseoSansCyrl_bold',sans-serif;
+    color: #333;`;
+  // forms[0].appendChild(statusMessage);
+  // statusMessage.textContent = loadMessage;
 
+
+  // forms.forEach((item) => {
+  //   let form = item;
+  //   form.addEventListener('submit', (event) => {
+  //     event.preventDefault();
+
+  //     form.appendChild(statusMessage);
+  //     statusMessage.textContent = loadMessage;
+
+  //     const formData = new FormData(form);
+  //     let body = {};
+  //     formData.forEach((val, key) => {
+  //       body[key] = val;
+  //     });
+
+  //     postData(body)
+  //       .then((response) => {
+  //         if (response.status !== 200) {
+  //           throw new Error('Status network not 200');
+  //         }
+  //         statusMessage.textContent = successMessage;
+  //       })
+  //       .catch(() => {
+  //         statusMessage.textContent = errorMessage;
+  //       })
+  //       .finally(() => {
+  //         const formInput = document.querySelectorAll('input');
+  //         for (let i = 0; i < formInput.length; i++) {
+  //           let input = formInput[i];
+  //           if (form.contains(input)) {
+  //             input.value = '';
+  //           }
+  //         };
+  //       })
+
+  //   });
+
+  // });
 
   document.body.addEventListener('submit', (event) => {
     event.preventDefault();
